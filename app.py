839bd733bcc4738957f7e9183054076494c98aa2
S-Kagwi <125998279+S-Kagwi@users.noreply.github.com>
2024-07-04 12:06:02 +0100

    updated the python script

--- app.py
+++ app.py
@@ -2,27 +2,64 @@ import streamlit as st
 import pandas as pd
 import pickle
 
+# Set the page configuration
+st.set_page_config(page_title='Hypertension Prediction', page_icon='🩺', layout='wide')
+
+# Inline CSS for styling
+
+st.markdown("""
+    <style>
+    .reportview-container {
+        background-color: #ffeb3b;  /* Bright yellow */
+        color: #333;
+    }
+    .sidebar .sidebar-content {
+        background-color: #03a9f4;  /* Light blue */
+    }
+    .stButton>button {
+        background-color: #8bc34a !important;  /* Light green */
+        color: white !important;
+        font-size: 20px !important;
+        padding: 10px 20px !important;
+        border-radius: 5px !important;
+        border: none !important;
+        cursor: pointer !important;
+        transition: background-color 0.3s ease !important;
+    }
+    .stButton>button:hover {
+        background-color: #4caf50 !important;  /* Green */
+    }
+    </style>
+    """, unsafe_allow_html=True)
+
 # Load the trained model
-model = pickle.load(open('finalized_model.pkl', 'rb'))
+@st.cache_resource
+def load_model():
+    try:
+        model = pickle.load(open('finalized_model.pkl', 'rb'))
+        return model
+    except Exception as e:
+        st.error(f"Error loading model: {e}")
+        return None
 
-# Main function for Streamlit app
-def main():
-    st.title("Hypertension Prediction WebApp")
+model = load_model()
 
-    # User inputs with descriptive text
-    age = st.number_input("What is your age?", min_value=0, max_value=100, step=1)
-    gender = st.selectbox("What is your gender?", ['Male', 'Female'])
-    smoking = st.selectbox("Do you smoke?", ['No', 'Yes'])
-    alcohol_consumption = st.selectbox("Do you consume alcohol?", ['No', 'Yes'])
-    obesity = st.selectbox("Are you obese?", ['No', 'Yes'])
-    family_history = st.selectbox("Does your family have a history of hypertension?", ['No', 'Yes'])
-    diabetes = st.selectbox("Do you have diabetes?", ['No', 'Yes'])
-    salt_intake = st.selectbox("How would you consider the level of your salt intake?", ['Low', 'High'])
-    diet = st.selectbox("How would you consider your diet to be?", ['Healthy','Unhealthy'])
-    stress_level = st.selectbox("Is your level of stress high or low?", ['Low', 'High'])
-    country = st.selectbox("Which country do you belong to?", ['South Africa', 'Nigeria'])
-
-    # Prepare input data for prediction
+# Function to get user input
+def get_user_input():
+    st.sidebar.header("Patient's Personal Information and Symptoms")
+    
+    age = st.sidebar.slider("What is your age?", min_value=0, max_value=100, step=1, key="age")
+    gender = st.sidebar.radio("What is your gender?", ['Male', 'Female'], key="gender")
+    smoking = st.sidebar.selectbox("Do you smoke?", ['No', 'Yes'], key="smoking")
+    alcohol_consumption = st.sidebar.selectbox("Do you consume alcohol?", ['No', 'Yes'], key="alcohol_consumption")
+    obesity = st.sidebar.selectbox("Are you obese?", ['No', 'Yes'], key="obesity")
+    family_history = st.sidebar.selectbox("Does your family have a history of hypertension?", ['No', 'Yes'], key="family_history")
+    diabetes = st.sidebar.selectbox("Do you have diabetes?", ['No', 'Yes'], key="diabetes")
+    salt_intake = st.sidebar.radio("How would you consider the level of your salt intake?", ['Low', 'High'], key="salt_intake")
+    diet = st.sidebar.radio("How would you consider your diet to be?", ['Healthy', 'Unhealthy'], key="diet")
+    stress_level = st.sidebar.radio("Is your level of stress high or low?", ['Low', 'High'], key="stress_level")
+    country = st.sidebar.radio("Which country do you belong to?", ['South Africa', 'Nigeria'], key="country")
+    
     input_features = {
         'AGE': age,
         'GENDER': 0 if gender == 'Male' else 1,
@@ -31,21 +68,33 @@ def main():
         'OBESITY': 1 if obesity == 'Yes' else 0,
         'FAMILY_HISTORY': 1 if family_history == 'Yes' else 0,
         'DIABETES': 1 if diabetes == 'Yes' else 0,
-        'SALT_INTAKE': 1 if salt_intake == 'Yes' else 0,
-        'DIET': {'Healthy': 0,'Unhealthy': 1}[diet],
-        'STRESS_LEVEL': {'Low': 0,'High': 1}[stress_level],
+        'SALT_INTAKE': 1 if salt_intake == 'High' else 0,
+        'DIET': {'Healthy': 0, 'Unhealthy': 1}[diet],
+        'STRESS_LEVEL': {'Low': 0, 'High': 1}[stress_level],
         'COUNTRY': 0 if country == 'South Africa' else 1
     }
+    
+    return input_features
 
-    # Prediction button
+# Main function for Streamlit app
+def main():
+    st.title("HyperPredict")
+    st.write("This app predicts the likelihood of a user having hypertension based on various health and lifestyle factors.")
+    st.image("pic.jpg", use_column_width=True)
+    
+    input_features = get_user_input()
+    
     if st.button('Predict Hypertension'):
-        input_df = pd.DataFrame([input_features])
-        prediction = model.predict(input_df)
-
-        if prediction[0] == 1:
-            st.success('The user is likely to be having hypertension.')
+        if model:
+            input_df = pd.DataFrame([input_features])
+            prediction = model.predict(input_df)
+            
+            if prediction[0] == 1:
+                st.success('The user is likely to have hypertension.')
+            else:
+                st.success('The user is not likely to have hypertension.')
         else:
-            st.success('The user is not likely to be having hypertension.')
+            st.error("Model not loaded correctly. Please try again later.")
 
 if __name__ == '__main__':
     main()
